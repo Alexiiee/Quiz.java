1fcf15301945896b4063a7f4df28794e3279e3e6
Alex <alexmonzy7@gmail.com>
2023-02-12 17:36:37 +0530

    following git commands are from stackoverflow

---
+++ app.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "lms.cvuouqo0nhv9.ap-south-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "Rose_9619608750",
+  database: "lms",
+});
+
+db.connect((err) =>{
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Database Connected sucessfully");
+});
