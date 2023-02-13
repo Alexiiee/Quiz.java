5f81d2ec330abb97c58548725f01a057da665496
Alex <alexmonzy7@gmail.com>
2023-02-13 19:44:00 +0530

    Adding DB connection to the stored repo for future use

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ db.connect((err) =>{
     return;
   }
   console.log("Database Connected sucessfully");
-  db.query("SELECT * FROM Login where ID > 40, function (err, result, fields) {
-    if(err) throw err;
+  db.query("SELECT * FROM Login where ID > 40", function (err, result, fields) {
+    if (err) throw err;
     console.log(result);
+  });
+
 });
-})
-conection .end();
